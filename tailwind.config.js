60a5763e03bf12302c6be10b66e068e1047b140a
Adrian <adrian.asensio.bdn@gmail.com>
2021-07-19 19:48:01 +0200

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
